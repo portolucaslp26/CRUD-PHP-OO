9173738701bf646d11b8df5422a8b7077daa3c17
Lucas Porto <68308175+portolucaslp26@users.noreply.github.com>
2022-02-06 12:48:36 -0300

    Add files via upload

---
+++ app/Db/Database.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Db;
+
+use \PDO;
+use \PDOException;
+
+class Database{
+
+  const HOST = 'localhost';
+
+  const NAME = 'wdev_vagas';
+
+  const USER = 'root';
+
+  const PASS = '';
+
+  private $table;
+
+  private $connection;
+
+  public function __construct($table = null){
+    $this->table = $table;
+    $this->setConnection();
+  }
+
+  /**
+   * Método responsável por criar uma conexão com o banco de dados
+   */
+  private function setConnection(){
+    try{
+      $this->connection = new PDO('mysql:host='.self::HOST.';dbname='.self::NAME,self::USER,self::PASS);
+      $this->connection->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    }catch(PDOException $e){
+      die('ERROR: '.$e->getMessage());
+    }
+  }
+
+  public function execute($query,$params = []){
+    try{
+      $statement = $this->connection->prepare($query);
+      $statement->execute($params);
+      return $statement;
+    }catch(PDOException $e){
+      die('ERROR: '.$e->getMessage());
+    }
+  }
+
+  public function insert($values){
+    //DADOS DA QUERY
+    $fields = array_keys($values);
+    $binds  = array_pad([],count($fields),'?');
+
+    //MONTA A QUERY
+    $query = 'INSERT INTO '.$this->table.' ('.implode(',',$fields).') VALUES ('.implode(',',$binds).')';
+
+    //EXECUTA O INSERT
+    $this->execute($query,array_values($values));
+
+    //RETORNA O ID INSERIDO
+    return $this->connection->lastInsertId();
+  }
+
+  public function select($where = null, $order = null, $limit = null, $fields = '*'){
+    //DADOS DA QUERY
+    $where = strlen($where) ? 'WHERE '.$where : '';
+    $order = strlen($order) ? 'ORDER BY '.$order : '';
+    $limit = strlen($limit) ? 'LIMIT '.$limit : '';
+
+    //MONTA A QUERY
+    $query = 'SELECT '.$fields.' FROM '.$this->table.' '.$where.' '.$order.' '.$limit;
+
+    //EXECUTA A QUERY
+    return $this->execute($query);
+  }
+
+  public function update($where,$values){
+    //DADOS DA QUERY
+    $fields = array_keys($values);
+
+    //MONTA A QUERY
+    $query = 'UPDATE '.$this->table.' SET '.implode('=?,',$fields).'=? WHERE '.$where;
+
+    //EXECUTAR A QUERY
+    $this->execute($query,array_values($values));
+
+    //RETORNA SUCESSO
+    return true;
+  }
+
+  public function delete($where){
+    //MONTA A QUERY
+    $query = 'DELETE FROM '.$this->table.' WHERE '.$where;
+
+    //EXECUTA A QUERY
+    $this->execute($query);
+
+    //RETORNA SUCESSO
+    return true;
+  }
+
+}
---
+++ app/Entity/Vaga.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Entity;
+
+use \App\Db\Database;
+use \PDO;
+
+class Vaga
+{
+  public $id;
+
+  public $titulo;
+
+  public $descricao;
+
+  public $ativo;
+
+  public $data;
+
+  public function cadastrar()
+  {
+    //DEFINIR A DATA
+    $this->data = date('Y-m-d H:i:s');
+
+    //INSERIR A VAGA NO BANCO
+    $obDatabase = new Database('vagas');
+    $this->id = $obDatabase->insert([
+      'titulo'    => $this->titulo,
+      'descricao' => $this->descricao,
+      'ativo'     => $this->ativo,
+      'data'      => $this->data
+    ]);
+
+    //RETORNAR SUCESSO
+    return true;
+  }
+
+  /**
+   * Método responsável por atualizar a vaga no banco
+   * @return boolean
+   */
+  public function atualizar()
+  {
+    return (new Database('vagas'))->update('id = ' . $this->id, [
+      'titulo'    => $this->titulo,
+      'descricao' => $this->descricao,
+      'ativo'     => $this->ativo,
+      'data'      => $this->data
+    ]);
+  }
+
+  public function excluir()
+  {
+    return (new Database('vagas'))->delete('id = ' . $this->id);
+  }
+
+  public static function getVagas($where = null, $order = null, $limit = null)
+  {
+    return (new Database('vagas'))->select($where, $order, $limit)
+      ->fetchAll(PDO::FETCH_CLASS, self::class);
+  }
+
+  public static function getVaga($id)
+  {
+    return (new Database('vagas'))->select('id = ' . $id)
+      ->fetchObject(self::class);
+  }
+}
